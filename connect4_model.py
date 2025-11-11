8dbebd88bcac09060a6d08286320f35955967a86
EmanueleGottardelli <128832775+EmanueleGottardelli@users.noreply.github.com>
2025-11-11 16:40:52 +0100

    modifiche ai model

--- connect4_model.py
+++ connect4_model.py
@@ -5,4 +5,5 @@ class Connect4Model(Model):
     def __init__(self, model=None):
         super().__init__(ninput=42,
                          layers=[64, 128, 128, 128, 128],
+                         n_actions=7,
                          model=model)
