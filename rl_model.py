98313fc944bc2b9acd9e57f8af4119c233605c48
EmanueleGottardelli <128832775+EmanueleGottardelli@users.noreply.github.com>
2025-11-13 17:57:42 +0100

    modifichine

--- rl_model.py
+++ rl_model.py
@@ -5,11 +5,6 @@ import numpy as np
 
 class Model:
     def __init__(self, ninput, layers, n_actions, model=None):
-        """
-        ninput: numero di input (celle dello stato)
-        layers: lista con il numero di neuroni per layer nascosto
-        n_actions: numero di azioni possibili
-        """
         self.keras_model = model or self.build_model(ninput, layers, n_actions)
 
     def build_model(self, ninput, layers, n_actions):
@@ -24,21 +19,12 @@ class Model:
         return model
 
     def predict(self, states):
-        """
-        states: lista di stati o singolo stato
-        restituisce array NumPy con Q-values
-        """
         states = np.array(states)
         if len(states.shape) == 1:  # singolo stato
             states = np.expand_dims(states, axis=0)
         return self.keras_model.predict(states, verbose=0)
 
     def fit(self, x, y, epochs=1, batch_size=32, verbose=0):
-        """
-        Funzione wrapper per allenare il modello
-        x: array di stati
-        y: array di target Q-values
-        """
         self.keras_model.fit(np.array(x), np.array(y),
                              epochs=epochs,
                              batch_size=batch_size,
